b71cac2c6cb1f6a0e04f363eb5ae48641f6570c0
prasan1h <prasanna.k.h.070@gmail.com>
2026-04-03 11:42:58 +0530

    controller added

---
+++ src/main/java/com/riderrr/app/Controller/UserController.java
@@ -0,0 +1,13 @@
+package com.riderrr.app.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/")
+    public String Home(){
+        return "Home";
+    }
+}
