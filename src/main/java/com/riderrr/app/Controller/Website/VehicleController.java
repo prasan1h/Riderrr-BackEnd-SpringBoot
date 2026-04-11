dd813add6551453385cefa55ddaf24249078bc78
2004Vishnu <vishnureddykoppad@gmail.com>
2026-04-11 12:38:01 +0530

    added localhost

--- src/main/java/com/riderrr/app/Controller/Website/VehicleController.java
+++ src/main/java/com/riderrr/app/Controller/Website/VehicleController.java
@@ -12,7 +12,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://127.0.0.1:5173/")
+@CrossOrigin(origins = {"http://localhost:5173","http://127.0.0.1:5173/"})
 public class VehicleController {
 
     @Autowired
