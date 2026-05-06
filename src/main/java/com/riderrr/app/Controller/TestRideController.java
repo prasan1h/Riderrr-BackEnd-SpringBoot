d28253e61b9a12600b5c5db1c9ed8103b1c46612
prasan1h <prasanna.k.h.070@gmail.com>
2026-05-06 15:45:05 +0530

    test  ride CORS update

--- src/main/java/com/riderrr/app/Controller/TestRideController.java
+++ src/main/java/com/riderrr/app/Controller/TestRideController.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://127.0.0.1:5173/")
+@CrossOrigin(origins = {"http://127.0.0.1:5173","http://localhost:5173"})
 @RequestMapping("/api/test-ride")
 public class TestRideController {
 
